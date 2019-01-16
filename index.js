c6c254e580437dced7c64ffe6ed0b7e180c3df47
Pierluigi Cau <pierluigi@github.com>
2019-01-16 16:23:16 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http')
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+  response.write('Hello Japan! 👋👋 \n\n')
+  response.write(`Server time: ${new Date(Date.now()).toLocaleString()}`)
+  response.end();
+})
+
+var port = process.env.PORT || 1337
+server.listen(port)
+
+
+// console.log('Server running at http://localhost:%d', port)
+
