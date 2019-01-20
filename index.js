5dcd6313c6ff9e22aed711558a24cf5c14160f18
Pierluigi Cau <pierluigi@github.com>
2019-01-20 18:55:16 +0000

    Fix

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var http = require("http");
-var utils = require("./lib/utils");
-var fs = require("fs");
+var http = require('http');
+var utils = require('./lib/utils');
+var fs = require('fs');
 
-var sha = fs.readFileSync("./deploy.sha", "utf8").trim();
+var sha = fs.readFileSync('./deploy.sha', 'utf8').trim();
 
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  const msg = utils.greetings("World");
-
-  response.write(msg + "\n\n");
-  response.write(
-    `Server time: ${new Date(Date.now()).toLocaleString()}` + "\n\n"
-  );
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  const msg = utils.greetings('World');
+  response.write(`${msg}\n\n`);
+  response.write(`Server time: ${new Date(Date.now()).toLocaleString()}\n\n`);
 
   if (sha.length) {
     response.write(`Deployed SHA: ${sha}`);
