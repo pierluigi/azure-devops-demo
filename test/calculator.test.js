498830e989a9ea054c1394395514f4f9ee33f03a
Pierluigi Cau <pierluigi@github.com>
2019-01-20 10:01:54 +0000

    Restore

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,4 +5,7 @@ describe("Calculator", function() {
   it("Says Hello in English", function() {
     assert.equal(greetings.sayHelloInEnglish(), "HELLO");
   });
+   it("Says Ciao in Italian", function() {
+    assert.equal(greetings.sayCiaoInItalian(), "CIAO");
+  });
 });
