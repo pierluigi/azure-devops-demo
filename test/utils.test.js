365e8998c679209f2ba369df9497007ba235abbd
Pierluigi Cau <pierluigi@github.com>
2019-01-21 11:01:32 +0000

    Fix lint

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,27 +1,27 @@
-var assert = require("assert");
-var utils = require("../lib/utils");
+var assert = require('assert');
+var utils = require('../lib/utils');
 
-describe("Utils", function() {
-  describe("greetings", function() {
-    it("Says Hello World", function() {
-      assert.equal(utils.greetings("World"), "Hello World 👋👋");
+describe('Utils', function () {
+  describe('greetings', function () {
+    it('Says Hello World', function () {
+      assert.equal(utils.greetings('World'), 'Hello World 👋👋');
     });
 
-    it("Throws on missing params", function() {
+    it('Throws on missing params', function () {
       assert.throws(() => {
         utils.greetings();
       }, Error);
     });
 
-    it("Throws on empty string", function() {
+    it('Throws on empty string', function () {
       assert.throws(() => {
-        utils.greetings("");
+        utils.greetings('');
       }, Error);
     });
 
-    it("Does not throw on a valid string", function() {
+    it('Does not throw on a valid string', function () {
       assert.doesNotThrow(() => {
-        utils.greetings("Valid!");
+        utils.greetings('Valid!');
       }, Error);
     });
   });
